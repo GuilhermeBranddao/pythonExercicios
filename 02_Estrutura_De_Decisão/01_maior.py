44944783f8afe801f82441e03dfffe606707a787
Guilherme Brandão <55138067+GuilhermeBranddao@users.noreply.github.com>
2020-06-02 09:46:26 -0300

    Update and rename exe02 to 01_maior.py

---
+++ 02_Estrutura_De_Decisão/01_maior.py
@@ -0,0 +1,3 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+"O maior numero é o {}".format(num1) if num1 > num2 else "O maior numero é o {}".format(num2)
