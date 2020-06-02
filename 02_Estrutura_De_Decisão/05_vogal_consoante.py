57a10ac0eb5e7236723d9656fab7fe228df2e0f3
Guilherme Brandão <55138067+GuilhermeBranddao@users.noreply.github.com>
2020-06-02 09:55:48 -0300

    Create 05_vogal_consoante.py

---
+++ 02_Estrutura_De_Decisão/05_vogal_consoante.py
@@ -0,0 +1,5 @@
+letra = str(input("Digite uma letra"))
+if(letra == "a" or letra == "b" or letra == "c" or letra == "d" or letra == "e" ):
+    print("A letra '{}' é uma vogal".format(letra))
+else:
+    print("A letra '{}' é uma consoante".format(letra))
